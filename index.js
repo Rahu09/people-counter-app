971e5c0312811d859db83fa918b022ebdc6672df
RAHU <tiwarirahul0809@gmail.com>
2023-02-07 00:23:30 +0530

    "first class"

---
+++ index.js
@@ -0,0 +1,13 @@
+// document.getElementById("count-el").innerText =5;
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch
+
+// console.log(count)
+
+let myage = 23
+let humanDogRatio = 7
+
+console.log(myage*humanDogRatio)
